a1727bb9fd72bd5a048b7333303c8113c60c516e
rpi <01158930@pw.edu.pl>
2024-04-21 09:40:00 +0200

    folder

--- rpi/1602_adafruit.py
+++ rpi/1602_adafruit.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2018 Mikey Sklar for Adafruit Industries
+                                    # SPDX-FileCopyrightText: 2018 Mikey Sklar for Adafruit Industries
 #
 # SPDX-License-Identifier: MIT
 # Modified by Jonathan Seyfert, 2022-01-22
